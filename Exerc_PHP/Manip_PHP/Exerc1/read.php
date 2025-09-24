020f04e27c7692ede37e2d1b2606def2bdf2ba0b
anajuliafernandes530-lang <anajuliafernandes530@gmail.com>
2025-09-23 23:17:50 -0300

    Delete Exerc_PHP/Manip_PHP directory

--- Exerc_PHP/Manip_PHP/Exerc1/read.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $file = fopen("file.txt", "r");
-
-    while(!feof($file)){
-        $line = fgets($file);
-        echo "$line <br>";
-    }
-    fclose($file);
-?>
--- Exerc_PHP/Manip_PHP/Exerc1/write.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $file = fopen("file.txt", "w");
-    fwrite($file, "Arquivo de texto");
-    fclose($file);
-?>
